1b191f8dacd4d6b9e663a9cfb75c8d64b0c5af1d
Cheremsha44 <r.chulkov44@gmail.com>
2026-06-19 12:38:34 +0500

    (js): class Todo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./styles.css";
+import { Todo } from "./todo.js";
+
+const task = new Todo('1', '2', '3', '4')
+task.toggleComplite()
+console.log(task)
---
+++ src/todo.js
@@ -0,0 +1,12 @@
+export class Todo{
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.isComplete = false;
+    };
+    toggleComplite(){
+        this.isComplete = !this.isComplete
+    }
+}
